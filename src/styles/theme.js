affb27ac9007712c3c0d5195b3302da6c6d18fe6
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-12-29 22:22:18 +0900

    style(App): Changed background color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
   palette: {
     background: {
-      default: '#eaeaea',
+      default: '#DEE3EB',
     },
   },
 });
